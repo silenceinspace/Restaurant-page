a5e09d5d5d387a769258173439e12b65e6993e12
Anton <anton20020426@gmail.com>
2023-06-22 18:41:45 +0200

    Import images with webpack

--- src/menu-page.js
+++ src/menu-page.js
@@ -1,4 +1,14 @@
 export { renderMenu };
+import borsch from "../src/images/menu/beef borsch.jpg";
+import vegeterianBorsch from "../src/images/menu/Vegeterian borsch.png";
+import kyivCutlet from "../src/images/menu/Kyiv cutlet.png";
+import holubtsi from "../src/images/menu/Holubtsi.png";
+import nalysnyky from "../src/images/menu/Nalysnyky.png";
+import varenyky from "../src/images/menu/Varenyky.png";
+import deruns from "../src/images/menu/Deruns.png";
+import salo from "../src/images/menu/Salo.png";
+import sweetVarenyky from "../src/images/menu/Sweet varenyky.png";
+import cheesecake from "../src/images/menu/Cheesecakes.png";
 
 function renderMenu(div) {
   // create html elements in DOM
@@ -48,12 +58,12 @@ function renderMenu(div) {
         case 0:
           header.textContent = "Beef borsch 0.4l";
           para.textContent = "Price - 99Czk";
-          image.src = "../src/images/menu/beef borsch.jpg";
+          image.src = borsch;
           break;
         case 1:
           header.textContent = "Vegetarian borscht with mushroom 0.4l";
           para.textContent = "Price - 99Czk";
-          image.src = "../src/images/menu/Vegeterian borsch.png";
+          image.src = vegeterianBorsch;
           break;
       }
       div.append(header, para, image);
@@ -74,33 +84,32 @@ function renderMenu(div) {
       case 0:
         header.textContent = "Kyiv cutlet with mashed potatoes";
         para.textContent = "Price - 245Czk";
-        img.src = "../src/images/menu/Kyiv cutlet.png";
+        img.src = kyivCutlet;
         break;
       case 1:
         header.textContent = "Holubtsi (cabbage rolls with meat)";
         para.textContent = "Price - 205Czk";
-        img.src = "../src/images/menu/Holubtsi.png";
+        img.src = holubtsi;
         break;
       case 2:
-        header.textContent =
-          "Nalysnyky stuffed with pork tongues";
+        header.textContent = "Nalysnyky stuffed with pork tongues";
         para.textContent = "Price - 180Czk";
-        img.src = "../src/images/menu/Nalysnyky.png";
+        img.src = nalysnyky;
         break;
       case 3:
         header.textContent = " Varenyky with potato filling";
         para.textContent = "Price - 110Czk";
-        img.src = "../src/images/menu/Varenyky.png";
+        img.src = varenyky;
         break;
       case 4:
         header.textContent = "Deruns with sour cream and salad";
         para.textContent = "Price - 90Czk";
-        img.src = "../src/images/menu/Deruns.png";
+        img.src = deruns;
         break;
       case 5:
         header.textContent = "Salo with borscht (two types of lard)";
         para.textContent = "Price - 70Czk";
-        img.src = "../src/images/menu/Salo.png";
+        img.src = salo;
         break;
     }
     mainCoursesDiv.append(div);
@@ -119,12 +128,12 @@ function renderMenu(div) {
       case 0:
         header.textContent = "Varenyky with sour cherries";
         para.textContent = "Price - 120Czk";
-        image.src = "../src/images/menu/Sweet varenyky.png";
+        image.src = sweetVarenyky;
         break;
       case 1:
         header.textContent = "Cottage cheese pancakes";
         para.textContent = "Price - 115Czk";
-        image.src = "../src/images/menu/Cheesecakes.png";
+        image.src = cheesecake;
         break;
     }
     dessertsDiv.append(div);
